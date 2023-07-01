cc04c7e9f25a844da034b1809dd3260286e076e8
shazaaly <shaza.aly@gmail.com>
2023-07-01 13:56:41 +0300

    doctest example calc

---
+++ tdd/doctest_ex.py
@@ -0,0 +1,30 @@
+class Calc:
+    """
+    A simple calc class
+    >>> c = Calc()
+    >>> c.add(3, 5)
+    8
+    >>> c.div(10, 0)
+    Traceback (most recent call last):
+        ...
+    ZeroDivisionError: division by zero
+
+    """
+    def add(self, a, b):
+        """Returns sum of 2 nums"""
+        return a + b
+
+    def div(self, a, b):
+        """
+        Returns div of 2 nums.
+        Raises:
+            ZeroDivisionError: if b is zero
+        """
+        if b == 0:
+            raise ZeroDivisionError("division by zero")
+        else:
+            return a / b
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod
