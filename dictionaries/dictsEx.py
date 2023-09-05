fc880da8814ec39c235364ae09e98145e8a209fb
shazaaly <shaza.aly@gmail.com>
2023-09-05 07:14:14 +0300

    dixtionaries

---
+++ dictionaries/dictsEx.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""dict examples"""
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+
+
+# Use ind_ger to print out capital of Germany
+print(countries[ind_ger])
